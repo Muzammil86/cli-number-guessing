0d6b725536102b241b1fbdacbc8e54db16636d60
Muzammil86 <160575864+Muzammil86@users.noreply.github.com>
2024-03-30 11:06:43 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([{
+        name: "userGuessNumber",
+        type: "number",
+        message: "please guess a number between 1 to 6:",
+    }]);
+if (answers.userGuessNumber === randomNumber) {
+    console.log("Congratulations! you guessed a right number.");
+}
+else {
+    console.log("You guessed wrong number");
+}
